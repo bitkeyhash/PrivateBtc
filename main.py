92ba29a543aa5ff1c678619163e76326d8cee0bd
BitKeyHash 🥷👨‍💻 <lefichier.com@gmail.com>
2025-02-08 18:21:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ init(autoreset=True)
 
 # Function to print ASCII logo with multiple colors
 def print_logo():
-    logo = """
+    logo = r"""
     ***********************************
     * ____       _            _       *
     *|  _ \ _ __(_)_   ____ _| |_ ___ *
